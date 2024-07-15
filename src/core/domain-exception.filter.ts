0ad7c821f2966d810911c05a81f40a0f2fc7d233
Seonzoo <boy672820@gmail.com>
2024-07-15 23:45:41 +0900

    ♻️ Refactoring: ApplicationError 제거, DomainError로 대체하여 글로벌 핸들링

--- src/core/domain-exception.filter.ts
+++ src/core/domain-exception.filter.ts
@@ -1,44 +1,49 @@
-import { ApplicationError, ApplicationErrorCode } from '@lib/errors';
+import { DomainError, DomainErrorCode } from '@lib/errors';
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 
-@Catch(ApplicationError)
-export class ApplicationErrorFilter implements ExceptionFilter {
-  catch(error: ApplicationError, host: ArgumentsHost) {
+@Catch(DomainError)
+export class DomainExceptionFilter implements ExceptionFilter {
+  catch(error: DomainError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<any>();
 
     switch (error.code) {
-      case ApplicationErrorCode.InvalidParameter:
+      case DomainErrorCode.InvalidParameter:
         return response.status(400).json({
           code: 400,
           message: error.message,
         });
-      case ApplicationErrorCode.NotFound:
+      case DomainErrorCode.NotFound:
         return response.status(404).json({
           code: 404,
           message: error.message,
         });
-      case ApplicationErrorCode.Duplicated:
+      case DomainErrorCode.Duplicated:
         return response.status(409).json({
           code: 409,
           message: error.message,
         });
-      case ApplicationErrorCode.Conflict:
+      case DomainErrorCode.Conflict:
         return response.status(409).json({
           code: 409,
           message: error.message,
         });
-      case ApplicationErrorCode.Unauthorized:
+      case DomainErrorCode.Unauthorized:
         return response.status(401).json({
           code: 401,
           message: error.message,
         });
-      case ApplicationErrorCode.Forbidden:
+      case DomainErrorCode.Forbidden:
         return response.status(403).json({
           code: 403,
           message: error.message,
         });
-      case ApplicationErrorCode.InternalError:
+      case DomainErrorCode.LimitExceeded:
+        return response.status(422).json({
+          code: 422,
+          message: error.message,
+        });
+      case DomainErrorCode.InternalError:
         return response.status(500).json({
           code: 500,
           message: error.message,
