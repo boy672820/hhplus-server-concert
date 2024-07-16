53e2371e06fe4b779a150c3cd6cfdddffd850d11
Seonzoo <boy672820@gmail.com>
2024-07-16 11:02:48 +0900

    Pay Request isEmpty -> isNotEmpty

--- src/interface/dto/requests/pay.request.ts
+++ src/interface/dto/requests/pay.request.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class PayRequest {
   @ApiProperty({
@@ -7,6 +7,6 @@ export class PayRequest {
     description: '예약 ID',
   })
   @IsString()
-  @IsEmpty()
+  @IsNotEmpty()
   public readonly reservationId: string;
 }
