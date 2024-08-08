385c9ea32417cfc91a87cbf800c2fde28df678cc
Seonzoo <boy672820@gmail.com>
2024-08-09 02:05:14 +0900

    ✨ Feat: 이벤트 처리 테스트를 위한 Mock API 추가

--- src/core/test-core.module.ts
+++ src/core/test-core.module.ts
@@ -8,6 +8,7 @@ import { validate } from './env.validator';
 import { DomainExceptionFilter } from './domain-exception.filter';
 import { LoggingInterceptor } from './logging.interceptor';
 import { LoggingModule } from './logging.module';
+import { MockApiModule } from '../lib/mock';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { LoggingModule } from './logging.module';
     TestDatabaseModule,
     RedisModule,
     LoggingModule,
+    MockApiModule,
   ],
   providers: [
     {
@@ -31,6 +33,6 @@ import { LoggingModule } from './logging.module';
       useClass: LoggingInterceptor,
     },
   ],
-  exports: [RedisModule, CqrsModule],
+  exports: [RedisModule, CqrsModule, MockApiModule],
 })
 export class TestCoreModule {}
