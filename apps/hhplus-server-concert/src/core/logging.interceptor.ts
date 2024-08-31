3953483520cb5b8c74d3102de65b9c8851ddd62a
Seonzoo <boy672820@gmail.com>
2024-08-31 16:32:34 +0900

    🔊 Update Interceptor logging: 지연시간 및 상세 메타데이터 추가

--- apps/hhplus-server-concert/src/core/logging.interceptor.ts
+++ apps/hhplus-server-concert/src/core/logging.interceptor.ts
@@ -1,7 +1,7 @@
 import { InjectLogger } from '@libs/logger/decorators';
 import { LoggerService } from '@libs/logger';
 import { Injectable, NestInterceptor } from '@nestjs/common';
-import { tap } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
@@ -10,19 +10,58 @@ export class LoggingInterceptor implements NestInterceptor {
   intercept(context, next) {
     const request = context.switchToHttp().getRequest();
 
+    const start = Date.now();
     return next.handle().pipe(
-      tap({
-        next: () =>
-          this.logger.http('Request success', 'LoggingInterceptor', {
+      tap(() => {
+        const response = context.switchToHttp().getResponse();
+        const delay = Date.now() - start;
+
+        this.logger.http('Request success', 'LoggingInterceptor', {
+          url: request.url,
+          method: request.method,
+          status: response.statusCode,
+          delay,
+        });
+
+        if (delay > 500) {
+          this.logger.warn('Request is slow (>500ms)', 'LoggingInterceptor', {
             url: request.url,
             method: request.method,
-          }),
-        error: (error) =>
-          this.logger.error(error.message, 'LoggingInterceptor', {
-            url: request.url,
-            method: request.method,
-            error,
-          }),
+            headers: request.headers,
+            body: request.body,
+            query: request.query,
+            params: request.params,
+            status: response.statusCode,
+            delay,
+          });
+        }
+      }),
+      catchError((_error) => {
+        const response = context.switchToHttp().getResponse();
+        const delay = Date.now() - start;
+
+        const error: Error | unknown =
+          _error instanceof Error
+            ? {
+                name: _error.name,
+                message: _error.message,
+                stack: _error.stack,
+              }
+            : _error || {};
+
+        this.logger.error('Request error', 'LoggingInterceptor', {
+          url: request.url,
+          method: request.method,
+          headers: request.headers,
+          body: request.body,
+          query: request.query,
+          params: request.params,
+          status: response.statusCode,
+          delay,
+          error,
+        });
+
+        return throwError(() => error);
       }),
     );
   }
