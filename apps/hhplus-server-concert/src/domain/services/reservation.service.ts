175aa3da88fafa618c431c70978f5bb136b9861d
Seonzoo <boy672820@gmail.com>
2024-09-07 11:26:10 +0900

    ♻️ 예약 생성 로직에서 불필요한 try-catch 제거

--- apps/hhplus-server-concert/src/domain/services/reservation.service.ts
+++ apps/hhplus-server-concert/src/domain/services/reservation.service.ts
@@ -37,64 +37,56 @@ export class ReservationService {
     userId: string;
     seatId: string;
   }): Promise<Reservation> {
-    try {
-      const seat = await this.seatRepository.findById(seatId);
+    const seat = await this.seatRepository.findById(seatId);
 
-      if (!seat) {
-        throw DomainError.notFound('좌석을 찾을 수 없습니다.');
-      }
-
-      if (seat.isNotAvailable()) {
-        throw DomainError.conflict('이미 예약된 좌석입니다.');
-      }
-
-      const event = await this.eventRepository.findById(seat.eventId);
-
-      if (!event) {
-        throw DomainError.notFound('이벤트를 찾을 수 없습니다.');
-      }
-
-      const schedule = await this.scheduleRepository.findById(seat.scheduleId);
-
-      if (!schedule) {
-        throw DomainError.notFound('스케줄을 찾을 수 없습니다.');
-      }
-
-      const reservation = this.reservationFactory.create({
-        userId,
-        seatId,
-        seatNumber: seat.number,
-        price: seat.price,
-        eventId: seat.eventId,
-        eventTitle: event.title,
-        eventAddress: event.address,
-        eventStartDate: event.startDate,
-        eventEndDate: event.endDate,
-        scheduleStartDate: schedule.startDate,
-        scheduleEndDate: schedule.endDate,
-      });
-
-      await this.reservationRepository.save(reservation);
-
-      const transaction = await this.outboxAdapter.publish(
-        EventType.ReservationReservedSeat,
-        ReservationReservedSeatEvent.toPayload({
-          reservationId: reservation.id,
-          seatId: seat.id,
-        }),
-      );
-
-      reservation.reserveSeat({ seatId, transactionId: transaction.id });
-      reservation.commit();
-
-      return reservation;
-    } catch (e) {
-      this.logger.error(
-        `[예약 생성 실패] 예약정보: ${JSON.stringify({ userId, seatId })}`,
-        e,
-      );
-      throw e;
+    if (!seat) {
+      throw DomainError.notFound('좌석을 찾을 수 없습니다.');
     }
+
+    if (seat.isNotAvailable()) {
+      throw DomainError.conflict('이미 예약된 좌석입니다.');
+    }
+
+    const event = await this.eventRepository.findById(seat.eventId);
+
+    if (!event) {
+      throw DomainError.notFound('이벤트를 찾을 수 없습니다.');
+    }
+
+    const schedule = await this.scheduleRepository.findById(seat.scheduleId);
+
+    if (!schedule) {
+      throw DomainError.notFound('스케줄을 찾을 수 없습니다.');
+    }
+
+    const reservation = this.reservationFactory.create({
+      userId,
+      seatId,
+      seatNumber: seat.number,
+      price: seat.price,
+      eventId: seat.eventId,
+      eventTitle: event.title,
+      eventAddress: event.address,
+      eventStartDate: event.startDate,
+      eventEndDate: event.endDate,
+      scheduleStartDate: schedule.startDate,
+      scheduleEndDate: schedule.endDate,
+    });
+
+    await this.reservationRepository.save(reservation);
+
+    const transaction = await this.outboxAdapter.publish(
+      EventType.ReservationReservedSeat,
+      ReservationReservedSeatEvent.toPayload({
+        reservationId: reservation.id,
+        seatId: seat.id,
+      }),
+    );
+
+    reservation.reserveSeat({ seatId, transactionId: transaction.id });
+    reservation.commit();
+
+    return reservation;
   }
 
   async pay({
